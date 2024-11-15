1ccc34b015ceebd33062dcd81c4b7601fb470428
llw <lonelyholiday0620@gmail.com>
2024-11-15 15:58:40 +0800

    更新2.0

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 # 1.0版本 
 1. 显示地图
 2. apk路径：./releaseApk/OpenMap1.0.apk 
+
+# 2.0版本
+1. 定位监听
+2. Marker 添加
+3. 地图点击
+4. apk路径：./releaseApk/OpenMap1.0.apk 
--- app/src/main/java/com/llw/openmap/MainActivity.kt
+++ app/src/main/java/com/llw/openmap/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.llw.openmap
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.llw.openmap.databinding.ActivityMainBinding
+import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.CustomZoomButtonsController.Visibility
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.MinimapOverlay
+import org.osmdroid.views.overlay.Overlay
+import org.osmdroid.views.overlay.ScaleBarOverlay
+import org.osmdroid.views.overlay.compass.CompassOverlay
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
+import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2
+import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,6 +46,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val TAG = "MainActivity"
+    // 是否定位
+    private var isLocation = false
+    // 定位管理器
+    private lateinit var locationManager: LocationManager
+    // 标记
+    private var mMarker: Marker? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,6 +64,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 创建位置管理器实例
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
     }
 
     override fun onResume() {
@@ -59,10 +89,49 @@ class MainActivity : AppCompatActivity() {
                 setZoom(12.0)
                 setCenter(Config.defaultGeoPoint)
             }
-            zoomController.setVisibility(Visibility.NEVER)
+            // 缩放控件显示-淡入淡出
+            zoomController.setVisibility(Visibility.SHOW_AND_FADEOUT)
+            this.isAnimating
             setMultiTouchControls(true)
-            overlayManager.tilesOverlay.isEnabled = true
+            // 覆盖管理器配置
+            overlayManager.apply {
+                tilesOverlay.isEnabled = true
+                add(object : Overlay() {
+                    override fun onSingleTapConfirmed(e: MotionEvent?, mapView: MapView?): Boolean {
+                        // 获取投影对象后进行坐标转换再切换地图中心位置
+                        mapView?.projection?.let { proj ->
+                            val geoPoint = proj.fromPixels(e!!.x.toInt(), e.y.toInt()) as GeoPoint
+                            Log.d(TAG, "onSingleTapConfirmed: 切换地图中心位置")
+                            changeMapCenter(geoPoint)
+                        }
+                        return super.onSingleTapConfirmed(e, mapView)
+                    }
+                })
+                // 添加比例尺
+                add(ScaleBarOverlay(binding.mapView).apply {
+                    setAlignBottom(true) // 底部对齐
+                    setScaleBarOffset(100, 10) // 设置偏移量
+                })
+                // 添加指南针
+                add(CompassOverlay(this@MainActivity, binding.mapView).apply {
+                    enableCompass()
+                })
+                // 添加经纬度网格线
+//                add(LatLonGridlineOverlay2())
+                // 启用旋转手势
+                add(RotationGestureOverlay(binding.mapView).apply { isEnabled = true })
+                // 添加小地图
+                add(MinimapOverlay(this@MainActivity, binding.mapView.tileRequestCompleteHandler).apply {
+                    val dm = resources.displayMetrics
+                    width = dm.widthPixels / 4
+                    height = dm.heightPixels / 4
+                    // 设置小地图资源
+                    setTileSource(Config.TDTCIA_W)
+                })
+            }
         }
+        // 开始定位
+        startLocation()
     }
 
     /**
@@ -85,5 +154,66 @@ class MainActivity : AppCompatActivity() {
      */
     private fun requestPermission() {
         permissionLauncher.launch(permissions)
+    }
+
+    /**
+     * 定位监听
+     */
+    private val locationListener = LocationListener { location ->
+        // 更新位置
+        val latitude = location.latitude
+        val longitude = location.longitude
+        // 更新地图中心点
+        changeMapCenter(GeoPoint(latitude, longitude))
+        // 停止定位
+        stopLocation()
+    }
+
+    /**
+     * 开始定位
+     */
+    @SuppressLint("MissingPermission")
+    private fun startLocation() {
+        if (!isLocation){
+            // 注册位置监听器
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0f, locationListener)
+            isLocation = !isLocation
+        }
+    }
+
+    /**
+     * 停止定位
+     */
+    private fun stopLocation() {
+        if (isLocation) {
+            // 停止位置更新
+            locationManager.removeUpdates(locationListener)
+            isLocation = !isLocation
+        }
+    }
+
+    /**
+     * 修改地图中心点
+     */
+    private fun changeMapCenter(geoPoint: GeoPoint) {
+        Log.d(TAG, "changeMapCenter: $geoPoint")
+        binding.mapView.apply {
+            // 移除标点
+            if (mMarker != null) {
+                overlays.remove(mMarker)
+            }
+            mMarker = Marker(this).apply {
+                title = "Marker"
+                position = geoPoint
+                icon = ContextCompat.getDrawable(this@MainActivity, R.drawable.ic_marker)
+            }
+            // 添加标点
+            overlays.add(mMarker)
+
+            controller.apply {
+                setZoom(14.0)
+                setCenter(geoPoint)
+            }
+        }
     }
 }
